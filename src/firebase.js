1604c542051bb03d30fa34af05a8051a6d8e598f
pal <82200759+pal404error@users.noreply.github.com>
2022-03-25 19:47:18 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,7 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  databaseURL: "https://notecafev2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  apiKey: "AIzaSyBcsc1YzNf6EEuI_m1vR-e0m2MaqYsZHfA",
-  authDomain: "notecafev2.firebaseapp.com",
-  projectId: "notecafev2",
-  storageBucket: "notecafev2.appspot.com",
-  messagingSenderId: "938464769996",
-  appId: "1:938464769996:web:a085ac37ee807d884c8f69",
-  measurementId: "G-RYLK81HF59"
+//config
 };
 
 firebase.initializeApp(firebaseConfig);
