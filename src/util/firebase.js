f21e85b81139822afd88f8fbd1dd63127a87684e
pal <82200759+pal404error@users.noreply.github.com>
2022-03-25 19:37:42 +0530

    Update firebase.js

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -6,13 +6,8 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBfI0LFYCN9AbtastKQ-XRCzdVW69_gb64",
-  authDomain: "notee-eb942.firebaseapp.com",
-  databaseURL: "https://notee-eb942-default-rtdb.firebaseio.com",
-  projectId: "notee-eb942",
-  storageBucket: "notee-eb942.appspot.com",
-  messagingSenderId: "1082831555961",
-  appId: "1:1082831555961:web:0411ac19cd73c9237f73a8"
+//config
+  
 };
 
 firebase.initializeApp(firebaseConfig);
