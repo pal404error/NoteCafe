d52995cb6c7583f4d8c39dd552ea568105456684
pal <82200759+pal404error@users.noreply.github.com>
2022-03-26 18:51:22 +0530

    26th updates

---
+++ src/components/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+const Header = props => (
+  <>
+         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"  />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <div class="w3-top">
+  <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+    <a href="#NOTES" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Notes</a>
+    <a href="#TODO" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">To-Do List</a>
+    <a href="#CHAT" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Community Chat</a>
+    <a href="#PDF" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Pdf Reader</a>
+  </div>
+</div>
+  <header className="header">
+
+    <h3 className="title">Take Notes</h3>
+  </header>
+</>
+);
+
+export default Header;
